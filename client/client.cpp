ebf91f8d984272f29a5d9e12f3e78a9540b244e2
zestze <eer2138@columbia.edu>
2018-01-09 18:54:44 -0600

    finished simple I/O and stuff for client side

--- client/client.cpp
+++ client/client.cpp
@@ -6,8 +6,11 @@
 
 #include <iostream>
 #include <boost/asio.hpp>
-#include <boost/array.hpp>
+#include <boost/array.hpp> // not needed
+#include <boost/algorithm/string.hpp>
 #include <string>
+#include <deque>
+#include <array>
 #include "user.h"
 #include "client.h"
 
@@ -17,6 +20,57 @@
 using boost::asio::ip::tcp;
 
 std::string RESERVED_CHARS[3] = {":", "!", "@"};
+
+std::deque<std::string> sock_msgs;
+
+void try_writing_to_sock(tcp::socket& sock, std::string msg)
+{
+	try {
+		if (msg.substr(msg.length() - 2, std::string::npos) != "\r\n")
+			throw std::invalid_argument("All IRC msgs need \\r\\n suffix");
+		boost::system::error_code ec;
+		boost::asio::write(sock, boost::asio::buffer(msg),
+				boost::asio::transfer_all(), ec);
+		// @TODO: implement proper async write, or have a timeout.
+		// this blocks until all in buffer is transmitted.
+	} catch (...) {
+		throw;
+	}
+}
+
+// @TODO: needs proper timeout mechanism.
+std::string try_reading_from_sock(tcp::socket& sock)
+{
+	try {
+		if (!sock_msgs.empty()) {
+			std::string retval = sock_msgs.front();
+			sock_msgs.pop_front();
+			return retval;
+		}
+
+		//std::string full_msg;
+		//boost::array<char, 128> buff;
+		//std::array<char, 128> buff;
+		//std::size_t len = sock.read_some(boost::asio::buffer(buff), ec);
+		std::vector<char> buff;
+		boost::system::error_code ec;
+		sock.read_some(boost::asio::buffer(buff), ec);
+		std::string full_msg(buff.begin(), buff.end());
+		std::vector<std::string> msgs;
+
+		boost::algorithm::split(msgs, full_msg, boost::is_any_of("\r\n"));
+		for (auto it = msgs.begin(); it != msgs.end(); ++it) {
+			if (*it != "")
+				sock_msgs.push_back(*it);
+		}
+
+		std::string retval = sock_msgs.front();
+		sock_msgs.pop_front();
+		return retval;
+	} catch (...) {
+		throw;
+	}
+}
 
 std::string to_cyan(std::string msg)
 {
@@ -47,6 +101,26 @@ User query_and_create()
 	std::cin >> real;
 
 	return User(nick, user, nick);
+}
+
+void pass_user_into_to_server(User this_user, tcp::socket& serv_sock)
+{
+	try {
+		// send NICK
+		std::string msg = "NICK " + this_user.nick + "\r\n";
+		try_writing_to_sock(serv_sock, msg);
+		// write to socket
+
+		// send USER; asterisks for ignored fields
+		msg  = "USER " + this_user.user_name + " * * :";
+		msg += this_user.real_name + "\r\n";
+		try_writing_to_sock(serv_sock, msg);
+
+		std::string reply = try_reading_from_sock(serv_sock);
+		// @TODO: check if reply has correct reply in it.
+	} catch (...) {
+		throw;
+	}
 }
 
 int main(int argc, char **argv)
@@ -86,6 +160,8 @@ int main(int argc, char **argv)
 		size_t len = serv_sock.read_some(boost::asio::buffer(buff), ec);
 		std::cout.write(buff.data(), len);
 		*/
+
+		pass_user_into_to_server(this_user, serv_sock);
 
 	}
 	catch (const std::exception& e)
